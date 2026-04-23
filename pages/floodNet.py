b68edb1b0aa101fca212bbed2891d09ea52b656b
Davoud Omarzadeh <78741322+David1627@users.noreply.github.com>
2026-04-23 17:07:38 +0200

    Update floodNet.py

--- pages/floodNet.py
+++ pages/floodNet.py
@@ -165,7 +165,7 @@ if CLIENT_ID and CLIENT_SECRET:
             c_lab1, c_lab2, c_lab3 = st.columns(3)
             lab_before = c_lab1.selectbox("Baseline", d_list, index=0)
             lab_after = c_lab2.selectbox("Crisis", d_list, index=1)
-            cmap_choice = c_lab3.selectbox("CMap", ["Accent","Accent_r","Blues"])
+            cmap_choice = c_lab3.selectbox("CMap", ["viridis", "magma", "Greys_r"])
             db_min, db_max = st.slider("Range (dB)", -35, 5, (-25, -5))
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3))
             d_left = 10 * np.log10(st.session_state.image_cache_s1[lab_before][:,:,0] + 1e-10)
